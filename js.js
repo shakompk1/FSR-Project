49e68083c3c1d41d49a061735a1bfae585e8e15e
Шахрияр Мирзаи <61832690+shakompk1@users.noreply.github.com>
2020-09-24 14:34:59 +0400

    page1 ,page2

--- js.js
+++ js.js
@@ -2,10 +2,12 @@ let webdevelopment = document.querySelectorAll('.servicesButton')[0];
 let design = document.querySelectorAll('.servicesButton')[1];
 let serviceInfo = document.querySelector('.serviceInfo');
 let serviceType = document.querySelectorAll('.serviceType');
+let logoService = document.querySelector('.logoService img');
+let serviceLogo = document.querySelectorAll('.serviceLogo');
 
 let text = [
     ["hello",'webdevelopment'],
-    ["hello",'webdevelopment']
+    ["hello",'Design']
 ];
 
 webdevelopment.addEventListener('click',function(){
@@ -13,6 +15,10 @@ webdevelopment.addEventListener('click',function(){
     for (let i = 0; i < text[0].length; i++) {
         serviceInfo.innerHTML += " " + text[0][i];        
     }
+    logoService.src="img/type.jpg";
+    serviceLogo.forEach((img,i)=>{
+        img.src = 'img/type.jpg';
+    });
 });
 
 design.addEventListener('click',function(){
@@ -20,11 +26,17 @@ design.addEventListener('click',function(){
     for (let i = 0; i < text[1].length; i++) {
         serviceInfo.innerHTML += " " + text[1][i];        
     }
+    logoService.src="img/dog.jpg";
+    serviceLogo.forEach((img,i)=>{
+        img.src = 'img/dog  .jpg';
+    });
+
 });
 
 serviceType.forEach((button,index)=>{
     button.addEventListener('click',()=>{
         let count;
+        let speed = 300;
         if(  index == 0){
             count = '100%';
         }else if(index == 1){
@@ -32,37 +44,43 @@ serviceType.forEach((button,index)=>{
         }else if(index == 2){
             count = '-100%';
         }
+
+
+
+
+
+        //animation
             serviceType.forEach((b,i)=>{
-                if(i==0){
+                if(i == 0){
                     b.animate([
                         // keyframes
                         { }, 
                         { transform: `translate3D(0, ${count}, 0)` }
                     ], {
                         // timing options
-                        duration: 500,
+                        duration: speed,
                         fill: 'forwards'
                     });
                 }
-                if(i==1){
+                if(i == 1){
                     b.animate([
                         // keyframes
                         { }, 
                         { transform: `translate3D(0, ${count}, 0)` }
                     ], {
                         // timing options
-                        duration: 500,
+                        duration: speed,
                         fill: 'forwards'
                     }); 
                 }
-                if(i==2){
+                if(i == 2){
                     b.animate([
                         // keyframes
                         { }, 
                         { transform: `translate3D(0, ${count}, 0)` }
                     ], {
                         // timing options
-                        duration: 500,
+                        duration: speed,
                         fill: 'forwards'
                     }); 
                 }
