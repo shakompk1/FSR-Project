070e657f74b5936ebdf374999dae74664206f01f
Шахрияр Мирзаи <61832690+shakompk1@users.noreply.github.com>
2020-10-15 19:47:55 +0400

    portfolio ready

--- portfolio.js
+++ portfolio.js
@@ -1,74 +1,109 @@
-let leftButton = document.querySelector('.portfolioMainBlockLeftButton');
-let rightButton = document.querySelector('.portfolioMainBlockRightButton');
+let leftButton = document.querySelector('.portfolioMainBlockLeftButton i');
+let rightButton = document.querySelector('.portfolioMainBlockRightButton i');
+let mainBlock = document.querySelector('.portfolioMainBlockInfo');
 let porfolio = document.querySelector('.porfolioShortinfo');
-let infoArr = ['img/ae.jpg','img/pes.jpg','img/type.jpg','img/ps.png','img/ae.jpg','img/ae.jpg','img/ae.jpg'];
+let infoArr = ['img/ae.jpg','img/pes.jpg','img/type.jpg','img/ps.png','img/ae.jpg','img/pes.jpg','img/type.jpg','img/dog.jpg','img/hamster.jpg','img/figma.png','img/ae.jpg','img/ae.jpg','img/ae.jpg','img/ae.jpg'];
+
+let circle = setInterval(() => {
+    moveElement()
+}, 5000);
+
+console.log(leftButton);
 let divArr = [];
+let divBlockArr = []
+let positionArr = [];
 let transform = 0;
-let position = 0;
+let canPushButton = true;
+let width = screen.width;
 
 leftButton.addEventListener('click',changePortfolioBlockPosition);
 rightButton.addEventListener('click',changePortfolioBlockPosition);
 
-
 function changePortfolioBlockPosition(index){
-    if(index.path[0].classList.contains('portfolioMainBlockLeftButton')){
-        moveElement(false);
-    }else{
+    if(index.path[0].classList.contains('fa-chevron-left')){
+        clearInterval(circle);
         moveElement(true);
+    }else{
+        clearInterval(circle);
+        moveElement(false);
     } 
 }
 
-function moveElement(pos){
-    transform += pos?200:-200
-    let potfolioShortBlockInfo = document.querySelectorAll('.potfolioShortBlockInfo');
-    potfolioShortBlockInfo.forEach((element ,index)=>{
-        // setInterval(()=>{
-        //     element.style.left = `${PositionLeft(element) + 1}px`
-        // },1000)
-        // element.animate([
-        //     { }, 
-        //     { transform: `translate3D(${transform}%, 0, 0)` }
-        // ], {
-        //     duration: 1000,
-        //     fill: 'forwards'
-        // });
-    });
-    DeleteElement(potfolioShortBlockInfo[potfolioShortBlockInfo.length -1]);
-    AddElement();
-}
-function AddElementToHHtml(){
-    for (let i = 0; i < 7; i++) {
-        porfolio.append(divArr[i]);  
+
+function moveElement(status){
+    if(canPushButton){
+        canPushButton = false;
+        transform = status?-168:168;
+        divArr.forEach((element ,index)=>{
+            element.animate([
+                { left : `${positionArr[index]}px`}, 
+                { left: `${positionArr[index] + transform}px` }
+            ], {
+                duration: 500,
+                fill: 'forwards'
+            });
+        });
+        setTimeout(()=>{
+            AddElement(status);
+            DeleteElement(status);  
+            canPushButton  = true;
+        },500);
     }
 }
-function AddElement(){
-    porfolio.prepend(divArr[6]);
+
+function AddElementToHtml(status){
+    positionArr = [];
+    porfolio.innerHTML = "";
+    let position =status?-316: -148;
+    for (let i = 0; i < 11; i++) {
+        if(i != 0){
+            position += 168;
+        }else if(status && i==0){
+            position += 168
+        }
+        for (let i = 0; i < divArr.length; i++) {
+            divArr[i].classList.remove('activePortfolio');
+        }
+        divArr[1].classList.add('activePortfolio');
+        positionArr.push(position);
+        divArr[i].style.left =`${position}px`;
+        divArr[i].style.transform = `translate3D(0, 0, 0)`;
+        porfolio.append(divArr[i]);  
+    }
+    ShowImgMainBlock();
 }
 
-function DeleteElement(element){
-    element.remove();
+function AddElement(status){
+    if(status){
+        divArr.push(divArr[0]);
+    }else{
+        divArr.unshift(divArr[divArr.length-1]);
+    }
+}
+
+function DeleteElement(status){
+    if(status){
+        divArr.shift();
+    }else{
+        divArr.pop();
+    }
+    AddElementToHtml(status)
 }
 
 function CreatElement(){
-    porfolio.innerHTML = "";
     for(let i = 0;i<infoArr.length;i++){
-        if(i != 0){
-            position += 20;
-        }
         let img = document.createElement('img');
         img.src = `${infoArr[i]}`;
         img.classList.add('potfolioShortBlockInfo');
-        img.style.left = `${position}%`
         divArr.push(img);
     }
+    AddElementToHtml();
 }
 
-function PositionLeft(element){
-    return element.offsetLeft;
+function ShowImgMainBlock(){
+    mainBlock.innerHTML ="";
+    let img = document.createElement('img');
+    img.src = divArr[1].src;
+    mainBlock.append(img);
 }
-function PositionRight(element){
-    return element.offsetLeft + element.offsetWidth;
-}
-
 CreatElement();
-AddElementToHHtml();
